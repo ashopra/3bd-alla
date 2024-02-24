da4cda185f15a2d2ec7a64cd46302f846b609267
ashopra <a.shopra@hotmail.com>
2024-02-24 23:17:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ app.get('/api/data',(req, res) =>{
   res.json({message:'your api response'});
 });
   
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
