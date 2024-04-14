091728d8f4fcd0ca0019681832943038e832d36f
ashopra <a.shopra@hotmail.com>
2024-04-14 16:53:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-/*import info from "./info.json" assert {type: "json"};
+import info from "./info.json" assert {type: "json"};
 const a = document.querySelector(".btn");
 a.onclick = function (){
 alert(info[1]);
-}*/
-//const express = require('express');
-//const cors = require('cors');
-import express from 'express';
-import cors from 'cors';
-const app = express();
-
-// Enable CORS for all origins
-app.use(cors());
-
-// Your other routes and middleware
-app.get('/api/data',(req, res) =>{
-  res.json({message:'your api response'});
-});
-  
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000');
-});
+}
