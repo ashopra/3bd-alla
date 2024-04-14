70c3b72cb64e2b263fd84ab891235a8cc8a7f3b7
ashopra <a.shopra@hotmail.com>
2024-04-14 18:31:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ import info from "./info.json" assert {type: "json"};
 const a = document.querySelector(".btn");
 const b = document.querySelector(".stuts");
 
-a.onclick = show(info[1]);
+a.onclick = oauthrize();
 function show(d){
   b.textContent = d;
+};
+function oauthrize(){
+  const url = 'https://twitter.com/i/oauth2/authorize';
+  const param = new URLSearchParams({
+  'response_type': 'code',
+  'client_id': 'M1M5R3BMVy13QmpScXkzTUt5OE46MTpjaQ',
+  'redirect_uri': 'https://www.example.com',
+  'scope': 'tweet.read users.read offline.access',
+  'state': 'state',
+  'code_challenge': 'challenge',
+  'code_challenge_method': 's256'
+    });
+  location.href = url + '?' + param;
 };
