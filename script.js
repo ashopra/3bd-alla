9f50a992552abe3606db6c2a6bfc5132a4304057
ashopra <a.shopra@hotmail.com>
2024-04-14 18:06:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import info from "./info.json" assert {type: "json"};
 const a = document.querySelector(".btn");
 const b = document.querySelector(".stuts");
-a.onclick = function (){
-  b.textContent = info[1];
+function show(d){
+  b.textContent = d;
 }
+a.onclick = show(info[1]);
